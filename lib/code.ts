244be25cd577aed33376c352e8b6765c4e712c85
Hai <hailv@magezon.com>
2024-04-05 15:06:30 +0700

    Add package and set width height for plugin

--- lib/code.ts
+++ lib/code.ts
@@ -7,7 +7,7 @@
 // full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__);
+figma.showUI(__html__, { width: 640, height: 480 });
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
