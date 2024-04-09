0a95fc15ffc050ca0b85ead31b6ad159952278f3
Hai <hailv@magezon.com>
2024-04-09 17:20:52 +0700

    add css for progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
 import { toHtml } from "hast-util-to-html";
 import "figma-plugin-ds/dist/figma-plugin-ds.css";
@@ -6,6 +6,10 @@ import Empty from "./components/Empty";
 import Code from "./components/Code";
 
 function App() {
+	const requestRef = useRef<number>(0);
+	const startTimeRef = useRef<number>(0);
+	const startCountRef = useRef(0);
+	const [process, setProcess] = useState(0);
 	const [sectionLength, setSectionLength] = useState(0);
 	const [url, setUrl] = useState("");
 	const [htmlOutput, setHTMLOutput] = useState("");
@@ -23,6 +27,7 @@ function App() {
 		const onWindowMessage = (e: MessageEvent) => {
 			const msg = e.data.pluginMessage;
 			if (msg.type === "loaded") {
+				handleCancel();
 				const { selectionLength, preview } = msg.data;
 				setSectionLength(selectionLength);
 				preview && setUrl(convertToUrl(preview));
@@ -43,13 +48,67 @@ function App() {
 		};
 	}, []);
 
+	useEffect(() => {
+		if (process >= 200) {
+			startCountRef.current = 0;
+			startTimeRef.current = 0;
+			cancelAnimationFrame(requestRef.current);
+		}
+	}, [process]);
+
+	const animateCount = (timestamp: number) => {
+		if (!startTimeRef.current) {
+			startTimeRef.current = timestamp;
+		}
+		const deltaTime = timestamp - startTimeRef.current;
+		const progress = deltaTime / 1000; // divide by 1000 to convert milliseconds to seconds
+		const nextCount = Math.round(startCountRef.current + progress * 99); // 99 is total steps implement for 1s
+		setProcess(nextCount);
+		requestRef.current = requestAnimationFrame(animateCount);
+	};
+
 	const handleGenerateCode = () => {
-		setTabSelected("code");
-		parent.postMessage({ pluginMessage: { type: "generate_code" } }, "*");
+		startCountRef.current = 0;
+		startTimeRef.current = 0;
+		requestRef.current = requestAnimationFrame(animateCount);
 	};
 
 	const handleSwapTab = (type: "preview" | "code") => {
 		setTabSelected(type);
+	};
+
+	const handleCancel = () => {
+		setProcess(0);
+		startCountRef.current = 0;
+		startTimeRef.current = 0;
+		cancelAnimationFrame(requestRef.current);
+	};
+
+	const handleGoToCode = () => {
+		setTabSelected("code");
+		parent.postMessage({ pluginMessage: { type: "generate_code" } }, "*");
+	};
+
+	const renderButtonAction = () => {
+		if (process >= 200)
+			return (
+				<button className="button button--primary" onClick={handleGoToCode}>
+					Go to Code
+				</button>
+			);
+
+		return process ? (
+			<button
+				className="button button--secondary button-cancel"
+				onClick={handleCancel}
+			>
+				Cancel
+			</button>
+		) : (
+			<button className="button button--primary" onClick={handleGenerateCode}>
+				Generate Code
+			</button>
+		);
 	};
 
 	const renderTabPreview = () =>
@@ -57,15 +116,18 @@ function App() {
 			<div className="container-preview">
 				<div className="wrapperImg">
 					<img alt="Preview Selection" src={url} />
+					{process > 0 && process < 200 && (
+						<div className="overlay-preview">
+							<div className="wrapper-progress-bar">
+								<div
+									className="progress-bar"
+									style={{ width: `${(process / 200) * 100}%` }}
+								/>
+							</div>
+						</div>
+					)}
 				</div>
-				<div className="button-generate">
-					<button
-						className="button button--primary"
-						onClick={handleGenerateCode}
-					>
-						Generate Code
-					</button>
-				</div>
+				<div className="button-generate">{renderButtonAction()}</div>
 			</div>
 		) : (
 			<Empty />
