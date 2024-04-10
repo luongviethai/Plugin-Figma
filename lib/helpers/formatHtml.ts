bfd61cd347b50554d38c23c461a22507cef75481
HaiLv <hailv@magezon.com>
2024-04-10 23:29:15 +0700

    add css logic

--- lib/helpers/formatHtml.ts
+++
@@ -1,9 +0,0 @@
-// import prettier from "prettier/standalone";
-// import parserHtml from "prettier/parser-html";
-
-// export function formatHTML(value: string): Promise<string> {
-// 	return prettier.format(value, {
-// 		parser: "html",
-// 		plugins: [parserHtml],
-// 	});
-// }
